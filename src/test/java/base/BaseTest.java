2ab94dd83cf5f69588e904d49a83451780c0e304
kabinja <renaudrwemalika@gmail.com>
2020-08-10 19:25:43 +0200

    chapter 13

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,12 +4,14 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.EventReporter;
 import utils.WindowManager;
 
 import java.io.File;
@@ -19,13 +21,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class BaseTest {
-    private WebDriver driver;
+    private EventFiringWebDriver driver;
     protected HomePage homePage;
 
     @BeforeClass
     public void setup(){
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new EventReporter());
 
         goHome();
 

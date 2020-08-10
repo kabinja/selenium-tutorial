dda9501086ebdac477664a67069942196319906b
kabinja <renaudrwemalika@gmail.com>
2020-08-10 19:19:56 +0200

    chapter 12

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,12 +1,22 @@
 package base;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import utils.WindowManager;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class BaseTest {
     private WebDriver driver;
@@ -25,6 +35,28 @@ public class BaseTest {
     @BeforeMethod
     public void goHome(){
         driver.get("http://the-internet.herokuapp.com/");
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result){
+        if(result.isSuccess()){
+            return;
+        }
+
+        final File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+
+        final Path targetPath = Paths.get("target","screenshots");
+        final File targetFolder = targetPath.toFile();
+        targetFolder.mkdirs();
+
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+
+        final String fileName = String.format("%s-%s.png", result.getName(), format.format(date));
+        final File destination = new File(targetFolder, fileName);
+
+        screenshot.renameTo(destination);
     }
 
     @AfterClass
